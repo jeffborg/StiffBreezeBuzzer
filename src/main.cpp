2d576221798edf31927fe66cf6b1b2bcd2b1df09
Jeffrey Borg <jeffrey@borgs.net>
2021-01-15 13:34:35 +1100

    added ota updating

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@
   /* ESP8266 Dependencies */
   #include <ESP8266WiFi.h>
   #include <ESPAsyncTCP.h>
+  #include <ArduinoOTA.h>
   #include <ESPAsyncWebServer.h>
 #elif defined(ESP32)
   /* ESP32 Dependencies */
@@ -93,11 +94,6 @@ float batteryVoltage;
 bool bInMenu = false;
 // new setting for seconds
 unsigned int newIntervalSeconds;
-
-// Used for attaching something to the lines, to make them focusable.
-void blankFunction() {
-    return;
-}
 
 // menu
 // Here the line is set to column 1, row 0 and will print the passed
@@ -268,6 +264,40 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  /* Arduino ota */
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
   /* Start AsyncWebServer */
   server.begin();
 
@@ -329,6 +359,7 @@ void setup() {
 }
 
 void loop() {
+  ArduinoOTA.handle();
   button.tick();
   timer.update();
   
